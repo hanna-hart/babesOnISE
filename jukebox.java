0e83660eb63e26a6631369e7d990a7ca1ee98976
Hanna Hart <hannaahart5@gmail.com>
2021-02-07 11:08:06 -0600

    added song panels and buttons

--- jukebox.java
+++ jukebox.java
@@ -16,14 +16,23 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class jukebox {
 
-	JLabel counterLabel, perSecLabel;
+	JLabel counterLabel, perSecLabel, songNameLabel;
+	JFrame window;
+	JPanel songNamePanel;
+	JButton fiftiesButton;
 	Font font1, font2;
 	int clickCount;
 	SongCountCounter songCounter = new SongCountCounter();
+	fiftiesAction fiftiesListener = new fiftiesAction();
+	sixtiesAction sixtiesListener = new sixtiesAction();
+	seventiesAction seventiesListener = new seventiesAction();
+	eightiesAction eightiesListener = new eightiesAction();
+	ninetiesAction ninetiesListener = new ninetiesAction();
 	
 	
 	public static void main(String[] args) {
@@ -46,25 +55,41 @@ public class jukebox {
 	
 	public void launch() {
 		// Create window and set size/color
-		JFrame window = new JFrame();
+		window = new JFrame();
 		window.setSize(800, 600);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.getContentPane().setBackground(Color.magenta);
+		window.getContentPane().setBackground(Color.gray);
 		window.setLayout(null);
 		
 		
 		// Create panel for jukebox picture
 		JPanel jukeboxPanel = new JPanel();
 		jukeboxPanel.setBounds(100,220,200,200);
-		jukeboxPanel.setBackground(Color.magenta);
+		jukeboxPanel.setBackground(Color.gray);
 		window.add(jukeboxPanel);
 		
 		// Panel for click count
 		JPanel counterPanel = new JPanel();
 		counterPanel.setBounds(100,100,250,100);
-		counterPanel.setBackground(Color.magenta);
+		counterPanel.setBackground(Color.gray);
 		counterPanel.setLayout(new GridLayout(2,1));
 		window.add(counterPanel);
+		
+		// Panel for 50s,60s,70s,80s,90s buttons
+		
+		JPanel songPanel = new JPanel();
+		songPanel.setBounds(400,170,320,250);
+		songPanel.setBackground(Color.gray);
+		songPanel.setLayout(new GridLayout(5,1));
+		window.add(songPanel);
+		
+		// Panel for song name
+		
+		songNamePanel = new JPanel();
+		songNamePanel.setBounds(100,450,400,50);
+		songNamePanel.setBackground(Color.gray);
+		window.add(songNamePanel);
+		
 		
 		// Initialize labels
 		
@@ -83,43 +108,149 @@ public class jukebox {
 		
 		// Create Buttons
 		JButton jukeboxButton = new JButton();
-		jukeboxButton.setBackground(Color.magenta);
+		jukeboxButton.setBackground(Color.gray);
 		jukeboxButton.setFocusPainted(false);
 		jukeboxButton.setIcon(jukebox);
 		jukeboxButton.addActionListener(songCounter);
 		jukeboxPanel.add(jukeboxButton);
 		
+		fiftiesButton = new JButton();
+		fiftiesButton.setBackground(Color.yellow);
+		songPanel.add(fiftiesButton);
 		
+		JButton sixtiesButton = new JButton();
+		sixtiesButton.setBackground(Color.yellow);
+		sixtiesButton.addActionListener(sixtiesListener);
+		songPanel.add(sixtiesButton);
+		
+		JButton seventiesButton = new JButton();
+		seventiesButton.setBackground(Color.yellow);
+		seventiesButton.addActionListener(seventiesListener);
+		songPanel.add(seventiesButton);
+		
+		JButton eightiesButton = new JButton();
+		eightiesButton.setBackground(Color.yellow);
+		eightiesButton.addActionListener(eightiesListener);
+		songPanel.add(eightiesButton);
+		
+		JButton ninetiesButton = new JButton();
+		ninetiesButton.setBackground(Color.yellow);
+		ninetiesButton.addActionListener(ninetiesListener);
+		songPanel.add(ninetiesButton);
+		
+		// Song Labels
+		JLabel fiftiesLabel = new JLabel("Song from the 50's (50 song credits)");
+		fiftiesLabel.setForeground(Color.black);
+		fiftiesLabel.setFont(font2);
+		fiftiesButton.addActionListener(fiftiesListener);
+		fiftiesButton.add(fiftiesLabel);
+		
+		JLabel sixtiesLabel = new JLabel("Song from the 60's (60 song credits)");
+		sixtiesLabel.setForeground(Color.black);
+		sixtiesLabel.setFont(font2);
+		sixtiesButton.add(sixtiesLabel);
+		
+		JLabel seventiesLabel = new JLabel("Song from the 70's (70 song credits)");
+		seventiesLabel.setForeground(Color.black);
+		seventiesLabel.setFont(font2);
+		seventiesButton.add(seventiesLabel);
+		
+		JLabel eightiesLabel = new JLabel("Song from the 80's (80 song credits)");
+		eightiesLabel.setForeground(Color.black);
+		eightiesLabel.setFont(font2);
+		eightiesButton.add(eightiesLabel);
+		
+		JLabel ninetiesLabel = new JLabel("Song from the 90's (90 song credits)");
+		ninetiesLabel.setForeground(Color.black);
+		ninetiesLabel.setFont(font2);
+		ninetiesButton.add(ninetiesLabel);
+		
+		songNameLabel = new JLabel();
+		songNameLabel.setForeground(Color.white);
+		songNameLabel.setFont(font2);
+		songNamePanel.add(songNameLabel);
 		
 		window.setVisible(true);
 		
 		
 	}
 	
-	public void playMusic() {
-	       
-		try{
-	    	     AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(this.getClass().getResource("C:\\Users\\hanna\\Documents\\OU\\Spring 2021\\Hacklahoma\\babesOnISE\\Fifties.mp3"));
-	    	     Clip clip = AudioSystem.getClip();
-	    	     clip.open(audioInputStream);
-	    	     clip.start( );
-	    	    }
-	    	   catch(Exception ex){  
-	    	   }
-
-	}
+//	public void playFiftiesMusic() {
+//	       
+//		try{
+//	    	     AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(this.getClass().getResource("C:\\Users\\hanna\\Documents\\OU\\Spring 2021\\Hacklahoma\\babesOnISE\\Fifties.mp3"));
+//	    	     Clip clip = AudioSystem.getClip();
+//	    	     clip.open(audioInputStream);
+//	    	     clip.start( );
+//	    	    }
+//	    	   catch(Exception ex){  
+//	    	   }
+//
+//	}
 	
 	public class SongCountCounter implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent event) {
 			clickCount++;
 			counterLabel.setText(clickCount + " song credits");
-			playMusic();
+			//playMusic();
+		}
+		
+	}
+	
+	public class fiftiesAction implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent event) {
+			if (clickCount >= 50) {
+				songNameLabel.setText("You are listening to 'I Got a Woman' by Ray Charles");
+				//playFiftiesMusic();
+			}
+		}
+	}
+	
+	public class sixtiesAction implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent event) {
+			if (clickCount >= 60) {
+				songNameLabel.setText("You are listening to 'I Got You' by James Brown");
+				//playFiftiesMusic();
+			}
+		}
+		
+	}
+	
+	public class seventiesAction implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent event) {
+			if (clickCount >= 7) {
+				songNameLabel.setText("You are listening to 'Don't Go Breaking My Heart' /n by Elton John (Feat. Kiki Dee)");
+				//playFiftiesMusic();
+			}
 		}
 		
 	}
 
-
+	public class eightiesAction implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent event) {
+			if (clickCount >= 80) {
+				songNameLabel.setText("You are listening to 'You Spin Me Round' by Dead or Alive");
+				//playFiftiesMusic();
+			}
+		}
+		
+	}
+	
+	public class ninetiesAction implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent event) {
+			if (clickCount >= 90) {
+				songNameLabel.setText("You are listening to 'Gettin' Jiggy Wit It' by Will Smith");
+				//playFiftiesMusic();
+			}
+		}
+		
+	}
 
 }
 
